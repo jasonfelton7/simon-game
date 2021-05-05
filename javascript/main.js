1dc297ef91cd12d0e3b20ec919da7fddf66ae32d
Jason <jfelton8899@gmail.com>
2021-05-04 22:18:27 -0400

    edited note

--- javascript/main.js
+++ javascript/main.js
@@ -236,4 +236,4 @@ function winGame() {
 }
 
 
-/* Thanks to Beau Carnes of FreeCodeCamp for the tutorial for the JavaScript. I made modifications to the HTML and CSS and recorded and used my own sounds, but the JavaScript is mainly as shown in the tutorial. */
+/* Thanks to Beau Carnes of FreeCodeCamp for the tutorial for the JavaScript. I made modifications to the HTML and CSS and recorded and used my own sounds. I also recorded a custom "error" sound for when you click the wrong color and added that to the JavaScript -- other than that, the JavaScript is mostly as shown in the tutorial. */
